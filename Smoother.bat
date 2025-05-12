003836d92322612aa0be5dde96d2e878b41e9d5d
Kyoware-Script <cravingherthoughts@gmail.com>
2025-05-12 19:56:30 +0100

    Add files via upload

---
+++ Smoother.bat
@@ -0,0 +1,27 @@
+@echo off
+title Kyoware V3 Loader [ BETA ]
+
+:1
+cls
+call :logo
+echo.
+echo Kyoware V3 
+goto 1
+
+:logo 
+chcp 65001 >nul
+echo [38;5;214m██╗  ██╗██╗   ██╗ ██████╗ ██╗    ██╗ █████╗ ██████╗ ███████╗
+echo [38;5;215m██║ ██╔╝╚██╗ ██╔╝██╔═══██╗██║    ██║██╔══██╗██╔══██╗██╔════╝
+echo [38;5;216m█████╔╝  ╚████╔╝ ██║   ██║██║ █╗ ██║███████║██████╔╝█████╗   
+echo [38;5;217m██╔═██╗   ╚██╔╝  ██║   ██║██║███╗██║██╔══██║██╔══██╗██╔══╝  
+echo [38;5;218m██║  ██╗   ██║   ╚██████╔╝╚███╔███╔╝██║  ██║██║  ██║███████╗
+echo [38;5;219m╚═╝  ╚═╝   ╚═╝    ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝[97m
+echo
+echo             Smoothing....
+echo               Smoothing Cheato For Better Preformance
+echo                     3
+echo                       2
+echo                         1
+echo
+echo
+echo                 [ Smoothed Enjoy! ]
